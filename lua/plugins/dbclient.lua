97e6a592c5503c4e221ea00708809160b6818322
Dawit Alemu <dndawita@gmail.com>
2024-05-29 11:18:16 -0400

    fix: remove transient prompt

---
+++ lua/plugins/dbclient.lua
@@ -0,0 +1,10 @@
+return {
+    "kndndrj/nvim-dbee",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    config = function()
+
+        require"dbee".setup()
+
+        vim.keymap.set("n", "<C-z>", "<cmd>lua require'dbee'.open()<CR>", { silent = true, noremap = true })
+    end
+}
