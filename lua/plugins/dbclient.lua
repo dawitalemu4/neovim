6c825d716e8d8eb54426b8218823bc563c229cc8
Dawit Alemu <dndawita@gmail.com>
2024-08-01 22:35:01 -0400

    add: added autoclose plugin

--- lua/plugins/dbclient.lua
+++ lua/plugins/dbclient.lua
@@ -3,7 +3,7 @@ return {
     dependencies = { "MunifTanjim/nui.nvim" },
     config = function()
 
-        require"dbee".setup()
+        require"dbee".setup({})
 
         vim.keymap.set("n", "<C-z>", "<cmd>lua require'dbee'.open()<CR>", { silent = true, noremap = true })
     end
